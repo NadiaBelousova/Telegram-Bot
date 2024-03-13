4e0baad7df816ec69f8f43c3f01204d8f6f5e913
Надежда <nadin94403@gmail.com>
2024-03-13 20:15:02 +0300

    начало проекта

---
+++ telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,46 @@
+package pro.sky.telegrambot.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+//            SendMessage message = new SendMessage(chatId, "messageText");
+//            String messageIncoming = "/start";
+            if (update.message().text().equals("/start")) {
+                Long chatId = update.message().chat().id();
+                String greetingMessage = "Hello! How can I help you?";
+
+            }
+            SendResponse response = telegramBot.execute(message);
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+}
